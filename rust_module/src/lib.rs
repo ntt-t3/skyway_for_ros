a61bc34d23a6e0e2e344f70f74827303c2477712
Toshiya Nakakura <nakakura@me.com>
2022-03-06 03:17:50 +0100

    set Logger

--- rust_module/src/lib.rs
+++ rust_module/src/lib.rs
@@ -6,3 +6,67 @@
 mod domain;
 mod error;
 mod infra;
+
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+use once_cell::sync::OnceCell;
+
+#[derive(Debug)]
+pub struct Logger {
+    pub debug_c: extern "C" fn(*const c_char) -> (),
+    pub info_c: extern "C" fn(*const c_char) -> (),
+    pub warn_c: extern "C" fn(*const c_char) -> (),
+    pub error_c: extern "C" fn(*const c_char) -> (),
+}
+
+impl Logger {
+    pub fn global() -> &'static Logger {
+        INSTANCE.get().expect("logger is not initialized")
+    }
+
+    fn debug(&self, message: impl Into<String>) {
+        let message_raw = CString::new(message.into()).unwrap().into_raw();
+        (self.debug_c)(message_raw);
+    }
+
+    fn info(&self, message: impl Into<String>) {
+        let message_raw = CString::new(message.into()).unwrap().into_raw();
+        (self.info_c)(message_raw);
+    }
+
+    fn warn(&self, message: impl Into<String>) {
+        let message_raw = CString::new(message.into()).unwrap().into_raw();
+        (self.warn_c)(message_raw);
+    }
+
+    fn error(&self, message: impl Into<String>) {
+        let message_raw = CString::new(message.into()).unwrap().into_raw();
+        (self.error_c)(message_raw);
+    }
+}
+
+static INSTANCE: OnceCell<Logger> = OnceCell::new();
+
+#[no_mangle]
+pub extern "C" fn register_logger(
+    debug_c: extern "C" fn(*const c_char),
+    info_c: extern "C" fn(*const c_char),
+    warn_c: extern "C" fn(*const c_char),
+    error_c: extern "C" fn(*const c_char),
+) {
+    INSTANCE
+        .set(Logger {
+            debug_c,
+            info_c,
+            warn_c,
+            error_c,
+        })
+        .unwrap();
+
+    let logger = Logger::global();
+    logger.debug("debug");
+    logger.info("info");
+    logger.warn("warn");
+    logger.error("error");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
+
+typedef void (*func_ptr_t)(char*);
+
+extern "C" {
+void log_debug_c(char* message) { ROS_DEBUG("%s", message); }
+
+void log_info_c(char* message) { ROS_INFO("%s", message); }
+
+void log_warn_c(char* message) { ROS_WARN("%s", message); }
+
+void log_err_c(char* message) { ROS_ERROR("%s", message); }
+
+void register_logger(func_ptr_t debug, func_ptr_t info, func_ptr_t warn,
+                     func_ptr_t error);
+}
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "skyway");
   ros::AsyncSpinner spinner(1);
+
   spinner.start();
 
-  ROS_INFO("running");
+  register_logger(log_debug_c, log_info_c, log_warn_c, log_err_c);
 
   spinner.stop();
   return 0;
