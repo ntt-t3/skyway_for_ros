605cdbe0c306757c97be9bccb4879002a368cd4c
Toshiya Nakakura <nakakura@me.com>
2022-09-02 07:39:36 +0200

    add udp_socket class

---
+++ src/socket/udp_socket.cpp
@@ -0,0 +1,106 @@
+//
+// Created by nakakura on 22/08/19.
+//
+
+#include "udp_socket.h"
+
+// UDPの受信スレッドを開始する
+// 重複コールは許容するが、stop後の再開はサポートしない(未定義動作)
+void UdpSocket::Start() {
+  if (is_running_) return;
+
+  if (!recv_thread_) {
+    socket_->open(udp::v4());
+    socket_->bind(local_endpoint_);
+
+    ros::NodeHandle nh_;
+
+    // io_service_->runは同期実行なので、別スレッドで行う
+    recv_thread_.reset(new std::thread([&] {
+      wait_for_packets();
+      io_service_->run();
+    }));
+  }
+
+  is_running_ = true;
+}
+
+// UDP受信スレッドを停止する
+// 重複コールは許容する
+// 各種サービスを停止し、threadのポインタをクリアする
+void UdpSocket::Stop() {
+  if (!is_running_) return;
+  if (socket_) {
+    socket_->cancel();
+    socket_->close();
+  }
+  if (recv_thread_) {
+    if (recv_thread_->joinable()) {
+      recv_thread_->join();
+    }
+    recv_thread_.reset();
+  }
+  if (io_service_) {
+    io_service_->stop();
+    io_service_.reset();
+  }
+
+  is_running_ = false;
+}
+
+// 内部で保持しているソケットのポート番号を取得する
+unsigned short UdpSocket::Port() {
+  if (socket_ && socket_->is_open()) {
+    return socket_->local_endpoint().port();
+  } else {
+    return 0;
+  }
+}
+
+void UdpSocket::SendData(std::vector<uint8_t> vec) {
+  socket_->async_send_to(
+      boost::asio::buffer(vec), target_socket_,
+      boost::bind(&UdpSocket::send_handler, this,
+                  boost::asio::placeholders::error,
+                  boost::asio::placeholders::bytes_transferred));
+}
+
+// ========== private methods ==========
+
+// boost::asioがasyncでデータ送信完了した際のcallback
+void UdpSocket::send_handler(const boost::system::error_code& error,
+                             std::size_t len) {
+  if (error.value() != boost::system::errc::success) {
+    ROS_WARN("fail to send data. %s", error.message().c_str());
+  }
+}
+
+// boost::asioがUDPパケットを受信した際に呼ばれるコールバックメソッド
+void UdpSocket::receive_handler(const boost::system::error_code& error,
+                                size_t bytes_transferred) {
+  if (error) {
+    if (error.message() != "Operation canceled") {
+      ROS_ERROR("Receive failed: %s", error.message().c_str());
+    }
+    return;
+  }
+
+  // 受信データの処理開始前に非同期受信を再開しておく
+  wait_for_packets();
+
+  std::vector<uint8_t> vec;
+  vec.insert(vec.end(), &recv_buffer_[0], &recv_buffer_[bytes_transferred]);
+  (*callback_)(vec);
+}
+
+// boost::asioで待受を開始する
+void UdpSocket::wait_for_packets() {
+  socket_.get();
+  if (socket_) {
+    socket_->async_receive_from(
+        boost::asio::buffer(recv_buffer_), target_socket_,
+        boost::bind(&UdpSocket::receive_handler, this,
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+  }
+}
---
+++ src/socket/udp_socket.h
@@ -0,0 +1,83 @@
+//
+// Created by nakakura on 22/08/19.
+//
+
+#ifndef SKYWAY_PLUGIN_UDP_PIPE_DATA_CHANNEL_PIPE_H
+#define SKYWAY_PLUGIN_UDP_PIPE_DATA_CHANNEL_PIPE_H
+
+#include <fruit/fruit.h>
+#include <ros/ros.h>
+
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <iostream>
+#include <thread>
+
+#include "socket.h"
+#include "std_msgs/UInt8MultiArray.h"
+
+using boost::asio::io_service;
+using boost::asio::ip::address;
+using boost::asio::ip::udp;
+using fruit::Component;
+using fruit::createComponent;
+using fruit::Injector;
+
+class UdpSocket : public Socket {
+ private:
+  std::shared_ptr<std::function<void(std::vector<uint8_t>)>> callback_;
+
+  // WebRTC GWのData Portの情報が格納される
+  udp::endpoint target_socket_;
+  // 送信エラーを管理する変数
+  boost::system::error_code err_;
+  // WebRTC GWからデータを受け取るためのポート情報とsocket
+  udp::endpoint local_endpoint_{};
+  std::unique_ptr<boost::asio::io_service> io_service_{new io_service()};
+  std::unique_ptr<udp::socket> socket_{
+      std::make_unique<udp::socket>(*io_service_.get())};
+  // WebRTC GWからデータを受信するスレッド
+  std::unique_ptr<std::thread> recv_thread_{nullptr};
+  // WebRTC GWから受信したUDPペイロードを格納するためのオブジェクト
+  // 1500(MTU) - 14(Ethernet Header) - 40(IPv6 Header) - 12(SCTP Header)
+  // = 1434bytesなので、Max 1434で用意する。
+  boost::array<uint8_t, 1434> recv_buffer_;
+  // start, stopメソッドの重複コールを避けるため利用するフラグ
+  bool is_running_ = false;
+
+  void send_handler(const boost::system::error_code &error, std::size_t len);
+  void receive_handler(const boost::system::error_code &error,
+                       size_t bytes_transferred);
+  void wait_for_packets();
+
+ public:
+  // デフォルトコンストラクタは削除
+  UdpSocket() = delete;
+  INJECT(UdpSocket(
+      ASSISTED(udp::endpoint) target_socket,
+      ASSISTED(std::shared_ptr<std::function<void(std::vector<uint8_t>)>>)
+          callback))
+      : target_socket_(target_socket), callback_(callback) {}
+  // デストラクタでは、PublisherとUDPポートの開放を行う
+  ~UdpSocket() { Stop(); }
+  // moveコンストラクタを追加
+  UdpSocket(UdpSocket &&) = default;
+  // WebRTC GWからのデータ受信とPublishを開始する
+  // 重複コールは許容するが、stop後の再開はサポートしない(未定義動作)
+  virtual void Start() override;
+  // WebRTC GWからのデータ受信とPublishを停止する
+  // 重複コールは許容する
+  virtual void Stop() override;
+  // 内部で保持しているソケットのポート番号を取得する
+  virtual unsigned short Port() override;
+  // socketからデータを送信する。非同期送信される
+  virtual void SendData(std::vector<uint8_t>) override;
+};
+
+using SocketFactory = std::function<std::unique_ptr<Socket>(
+    udp::endpoint, std::shared_ptr<std::function<void(std::vector<uint8_t>)>>)>;
+
+fruit::Component<SocketFactory> getSourceComponent();
+
+#endif  // SKYWAY_PLUGIN_UDP_PIPE_DATA_CHANNEL_PIPE_H
