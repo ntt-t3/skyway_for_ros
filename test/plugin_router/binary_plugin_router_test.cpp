20ba3f2b0ad3e0c158f9c1e625b063efa37912a1
Toshiya Nakakura <nakakura@me.com>
2023-04-06 18:50:33 +0900

    Add a message for testers to tests that output error messages

--- test/plugin_router/binary_plugin_router_test.cpp
+++ test/plugin_router/binary_plugin_router_test.cpp
@@ -90,6 +90,7 @@ TEST(TestSuite, binary_plugin_try_start_not_found_plugin) {
       "[{\"plugin_name\":\"binary_loopback::BinaryLoopback\",\"param\":"
       "\"Parameter\"},{"
       "\"plugin_name\":\"not_found_plugin::NotFoundPlugin\"}]");
+  ROS_INFO("This test produces a ROS_ERROR message because of a loading error in the plugin, but the behavior is as expected.");
 
   // objectを作成し、受信スレッドを開始
   Injector<Annotated<BinaryAnnotation, PluginRouterFactory>> injector(
